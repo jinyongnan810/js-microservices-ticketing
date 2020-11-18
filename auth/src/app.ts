0ca89fed52cc4620e9d850726c34a08e8b0a2b96
Yuunan Kin <yuunan.kin@gmail.com>
2020-11-18 14:15:15 +0900

    refactor index.ts for supertest

---
+++ auth/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+require("express-async-errors");
+import { json } from "body-parser";
+
+import cookieSesion from "cookie-session";
+
+import CurrentUserRouter from "./routers/current-user";
+import SignInRouter from "./routers/signin";
+import SignOutRouter from "./routers/signout";
+import SignUpRouter from "./routers/signup";
+import { handleError } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+
+const app = express();
+app.set("trust proxy", true); //trust ingress nginx
+app.use(json());
+app.use(
+  cookieSesion({
+    signed: false, // no encryption
+    secure: true, // only https
+  })
+);
+
+app.use(CurrentUserRouter);
+app.use(SignInRouter);
+app.use(SignOutRouter);
+app.use(SignUpRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(handleError);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,5 @@
-import express from "express";
-require("express-async-errors");
-import { json } from "body-parser";
 import mongoose from "mongoose";
-import cookieSesion from "cookie-session";
-
-import CurrentUserRouter from "./routers/current-user";
-import SignInRouter from "./routers/signin";
-import SignOutRouter from "./routers/signout";
-import SignUpRouter from "./routers/signup";
-import { handleError } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-const app = express();
-app.set("trust proxy", true); //trust ingress nginx
-app.use(json());
-app.use(
-  cookieSesion({
-    signed: false, // no encryption
-    secure: true, // only https
-  })
-);
-
-app.use(CurrentUserRouter);
-app.use(SignInRouter);
-app.use(SignOutRouter);
-app.use(SignUpRouter);
-
-app.all("*", async (req, res) => {
-  throw new NotFoundError();
-});
-
-app.use(handleError);
-
+import { app } from "./app";
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY not set.");
