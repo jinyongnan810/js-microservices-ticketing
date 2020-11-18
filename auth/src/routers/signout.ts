1a8db17094d1b1320af3bf5836ae870d7917ae1c
Yuunan Kin <yuunan.kin@gmail.com>
2020-11-18 10:25:46 +0900

    sign out

--- auth/src/routers/signout.ts
+++ auth/src/routers/signout.ts
@@ -1,6 +1,7 @@
-import express,{Request,Response} from 'express'
+import express, { Request, Response } from "express";
 const router = express.Router();
-router.get('/api/users/signout',(req:Request,res:Response)=>{
-    res.send({msg:'/api/users/signouthhh'})
-})
+router.post("/api/users/signout", (req: Request, res: Response) => {
+  req.session = null;
+  res.send({});
+});
 export default router;
