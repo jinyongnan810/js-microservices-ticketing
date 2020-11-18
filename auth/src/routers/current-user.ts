7ccf3f1bc268ccedbbbf75d9f5ab1c2d014c59c5
Yuunan Kin <yuunan.kin@gmail.com>
2020-11-18 10:51:39 +0900

    getCurrentUser and requireAuth middlewares

--- auth/src/routers/current-user.ts
+++ auth/src/routers/current-user.ts
@@ -1,19 +1,16 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { BadRequestError } from "../errors/bad-request-error";
+import { currentUser } from "../middlewares/current-user";
+import { requireAuth } from "../middlewares/require-auth";
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req: Request, res: Response) => {
-  const jwt_token = req.session?.jwt;
-  if (jwt_token) {
-    try {
-      const payload = jwt.verify(jwt_token, process.env.JWT_KEY!);
-      return res.send({ currentUser: payload });
-    } catch (error) {
-      return res.send({ currentUser: null });
-    }
+router.get(
+  "/api/users/currentuser",
+  currentUser,
+  (req: Request, res: Response) => {
+    return res.send({ currentUser: req.currentUser || null });
   }
-  return res.send({ currentUser: null });
-});
+);
 
 export default router;
