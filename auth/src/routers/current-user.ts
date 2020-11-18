8d8c149fbd33e064a6860acc8c98dc555e5a303b
Yuunan Kin <yuunan.kin@gmail.com>
2020-11-18 10:22:33 +0900

    current user

--- auth/src/routers/current-user.ts
+++ auth/src/routers/current-user.ts
@@ -1,8 +1,19 @@
-import express,{Request,Response} from 'express'
-const router = express.Router()
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { BadRequestError } from "../errors/bad-request-error";
+const router = express.Router();
 
-router.get('/api/users/currentuser',(req:Request,res:Response)=>{
-    res.send({msg:'/api/users/currentuser'})
-})
+router.get("/api/users/currentuser", (req: Request, res: Response) => {
+  const jwt_token = req.session?.jwt;
+  if (jwt_token) {
+    try {
+      const payload = jwt.verify(jwt_token, process.env.JWT_KEY!);
+      return res.send({ currentUser: payload });
+    } catch (error) {
+      return res.send({ currentUser: null });
+    }
+  }
+  return res.send({ currentUser: null });
+});
 
 export default router;
