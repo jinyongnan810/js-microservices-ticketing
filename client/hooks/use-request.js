8595d02eddaa7411972774cecb96fb38d31ed897
Yuunan Kin <yuunan.kin@gmail.com>
2020-11-22 09:50:02 +0900

    onSuccess callback to useRequest hook and redirect

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 import { useState } from "react";
 
-const UseRequest = (url, method, body) => {
+const UseRequest = (url, method, body, onSuccess) => {
   const [errors, setErrors] = useState(null);
   const doRequest = async () => {
     try {
       setErrors(null);
       const res = await axios[method](url, body);
+      if (onSuccess) {
+        onSuccess(res.data);
+      }
       return res.data;
     } catch (error) {
       setErrors(
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "axios";
+import Router from "next/router";
 import useRequest from "../../hooks/use-request";
 
 const SignupPage = () => {
@@ -11,7 +11,8 @@ const SignupPage = () => {
     {
       email,
       password,
-    }
+    },
+    () => Router.push("/")
   );
   const signUp = async (event) => {
     event.preventDefault();
