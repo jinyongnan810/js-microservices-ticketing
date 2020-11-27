2017d0860ea979121ab620cf237f209fdba55857
Yuunan Kin <yuunan.kin@gmail.com>
2020-11-27 10:21:58 +0900

    set to manual acknowledgement

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -6,11 +6,14 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 });
 stan.on("connect", () => {
   console.log("Listener connected to nats");
+  const options = stan.subscriptionOptions().setManualAckMode(true); //set manual acknowledge mode to true. Need to manually ack the event after save to db .etc.
+  // to prevent failed event get lost
   stan
-    .subscribe("ticket-created", "tickets-queue-group")
+    .subscribe("ticket-created", "tickets-queue-group", options)
     .on("message", (msg: Message) => {
       console.log(
         `Event received: Channel[${msg.getSubject()}],Seq[${msg.getSequence()}],Data[${msg.getData()}]`
       );
+      msg.ack(); // manually ack the event
     });
 });
