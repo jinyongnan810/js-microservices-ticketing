6293049583047088862b98c05db6e23a651d234c
Yuunan Kin <yuunan.kin@gmail.com>
2020-12-29 16:12:37 +0900

    make publisher async using promise

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -2,14 +2,14 @@ import nats from "node-nats-streaming";
 import { TicketCreatedPublisher } from "./events/ticket-create-publisher";
 console.clear();
 const stan = nats.connect("ticketing", "abc", { url: "http://localhost:4222" });
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publisher connected to nats");
   const data = {
     id: "123",
     title: "ticket1",
     price: 77.11,
   };
-  new TicketCreatedPublisher(stan).publish(data);
+  await new TicketCreatedPublisher(stan).publish(data);
   stan.on("close", () => {
     console.log("Publisher Closing...");
     process.exit();
