51b1e3425f4f7c59ada8bb71923c894fdd20cf9f
Yuunan Kin <yuunan.kin@gmail.com>
2021-01-02 11:59:22 +0900

    order service initial setup

---
+++ orders/src/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+require("express-async-errors");
+import { json } from "body-parser";
+
+import cookieSesion from "cookie-session";
+
+import { currentUser, handleError } from "@jinyongnan810/ticketing-common";
+import { NotFoundError } from "@jinyongnan810/ticketing-common";
+
+const app = express();
+app.set("trust proxy", true); //trust ingress nginx
+app.use(json());
+app.use(
+  cookieSesion({
+    signed: false, // no encryption
+    secure: process.env.NODE_ENV !== "test", // only https
+  })
+);
+
+// get auth info
+app.use(currentUser);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(handleError);
+
+export { app };
