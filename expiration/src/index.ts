d0f263c57d8e5ece01a43b0e164a89f7e3dce611
Yuunan Kin <yuunan.kin@gmail.com>
2021-01-12 08:45:38 +0900

    expiration service base setup

---
+++ expiration/src/index.ts
@@ -0,0 +1,32 @@
+import { randomBytes } from "crypto";
+import { natsWrapper } from "./events/nats-wrapper";
+
+const start = async () => {
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID not set.");
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID not set.");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL not set.");
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID!,
+      process.env.NATS_CLIENT_ID!,
+      process.env.NATS_URL!
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS client closed...");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+  } catch (error) {
+    console.log(error.messsage);
+  }
+};
+
+start();
